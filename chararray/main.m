1ef7f0cd10f68a227967c00a9fc4969d601759e4
Dowlath <hibrise@IKRAMs-Mac-mini.local>
2014-08-14 16:54:36 +0530

    chararray

--- chararray/main.m
+++ chararray/main.m
@@ -12,9 +12,61 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+     
+        //GIVING DEFAULT CHARACTER
+        int i;
+        char a[5]={'a','b','c','d','e'} ;
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+                for (i=0; i<=5; i++) {
+        }
+        for (i=0; i<=5; i++) {
+            NSLog(@"the answer is %c",a[i]);
+        }
+        
+ 
+    //GETTING CHARACTER FROM THE USER
+//        
+//        int i;
+//        char a[5];
+//        for(i=0;i<=5;i++)
+//        {
+//            NSLog(@"enter the character");
+//            scanf("%c",&a[i]);
+//        }
+//        for(i=0;i<=5;i++)
+//        {
+//            NSLog(@"the character is %c",a[i]);
+//     
+//        }
+        
+   //GETTING ITERATION AND CHARACTER FROM THE USER
+        
+        
+        
+//       int i,n;
+//       char a[10];
+//        NSLog(@"ENTER THE NUMBER");
+//        scanf("%d",&n);
+//        // printf("enter the character");
+//        for(i=0;i<=n;i++)
+//        {
+//           
+//            NSLog(@"enter the character");
+//            scanf("%c",&a[i]);
+//        }
+//        for(i=0;i<=n;i++)
+//        {
+//            printf("the character is %c",a[i]);
+//            
+//        }
+//
+        
+        
+        
+        
+        
+        
         
     }
     return 0;
